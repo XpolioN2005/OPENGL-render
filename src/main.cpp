ce4c2b4711942e83f13216173dafcd3bb0293689
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-16 22:42:02 +0530

    feat: Implement EBO for indexed drawing and add wireframe mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+bool WIREFRAME_MODE = false;
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -93,32 +95,39 @@ int main() {
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-        // Upper triangle
-        -0.5f, 0.5f, 0.0f, // top-left
-        0.5f, 0.5f, 0.0f,  // top-right
-        0.0f, 0.0f, 0.0f,  // center
-
-        // Lower triangle
-        -0.5f, -0.5f, 0.0f, // bottom-left
-        0.5f, -0.5f, 0.0f,  // bottom-right
-        0.0f, 0.0f, 0.0f    // center
+        0.5f,  0.5f,  0.0f, // top right
+        0.5f,  -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f,  0.0f  // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first Triangle
+        1, 2, 3  // second Triangle
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    glViewport(0, 0, 800, 600);
+    // wireframe
+    if (WIREFRAME_MODE) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -129,7 +138,9 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -137,6 +148,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
